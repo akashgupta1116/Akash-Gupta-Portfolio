cc62b899be53401680db6e11e70bb7e2215ab27b
akashgupta1116 <yoakash6@gmail.com>
2023-10-19 10:16:51 +0530

    disable deprecated websites

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,41 +1,59 @@
-import React from 'react';
-import './projects.css';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import details from '../projectDetails';
+import React from "react";
+import "./projects.css";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import details from "../projectDetails";
+import Tooltip from "@material-ui/core/Tooltip";
 
-const Projects =()=> {
-        return (
-            <div className="container">
-                <div className="heading">
-                    <h1>Projects</h1>
+const Projects = () => {
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>Projects</h1>
+      </div>
+
+      <div className="projects-bottom load-animation">
+        {details.map((item, index) => (
+          <Tooltip
+            title={item.isDeprecated ? "This app is deprecated" : ""}
+            followCursor
+            disableInteractive
+          >
+            <div
+              key={item + index}
+              className={"card" + (item.isDeprecated ? " grey-on-hover" : "")}
+            >
+              <div className="card-top">
+                <h4 className="project-name">{item.name}</h4>
+                <div className="github-icon-div">
+                  <a href={item.link} target="blank">
+                    <GitHubIcon />
+                  </a>
                 </div>
-               
-                <div className="projects-bottom load-animation">
-                    {details.map((item,index)=>(
-                        <div key={item+index} className="card">
-                            <div className="card-top">
-                                <h4 className="project-name">{item.name}</h4>
-                                <div className="github-icon-div">
-                                   <a href={item.link} target="blank"><GitHubIcon/></a>
-                                </div>
-                            </div>
-                            <hr/>
-                            <div className="tech-div">
-                                {item.tech.map(tech=>(
-                                    <span key={tech} className="tech-span">{tech}</span>
-                                ))}
-                            </div>
-                            
-                            <div className="project-image-part">
-                                <a href={item.deployedLink} target="blank"><img className="project-image" src={item.imgSrc} alt={item.name}/></a>
-                            </div>
-                            
-                        </div>
-                    ))}
-                </div>
+              </div>
+              <hr />
+              <div className="tech-div">
+                {item.tech.map((tech) => (
+                  <span key={tech} className="tech-span">
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
+              <div className="project-image-part">
+                <a href={item.deployedLink} target="blank">
+                  <img
+                    className="project-image"
+                    src={item.imgSrc}
+                    alt={item.name}
+                  />
+                </a>
+              </div>
             </div>
-        );
-    
-}
+          </Tooltip>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Projects;
--- src/projectDetails.js
+++ src/projectDetails.js
@@ -1,35 +1,47 @@
-const details = [{
-    name: 'Realtime Voting App',
-    tech: ['HTML', 'CSS', 'Javascript', 'React.js','NodeJS','ChartJS'],
-    link: 'https://github.com/akashgupta1116/realtime-voting-reactjs',
-    imgSrc: "https://drive.google.com/thumbnail?id=1c20PuvkW3XZrEHDUonDGp-bJ-HR5CmSs",
-    deployedLink: 'https://realtime-voting-react-ably.herokuapp.com/'
-},{
-    name: 'Akash Gupta',
-    tech: ['HTML', 'CSS', 'Javascript', 'React.js'],
-    link: 'https://github.com/akashgupta111/Covid-Tracker',
-    imgSrc: "https://drive.google.com/thumbnail?id=1rrWRSxIkP8v3spi-R64KNJMpwFx-aTJp",
-    deployedLink: 'https://akashgupta-portfolio.netlify.app/#/'
-}, {
-    name: 'India Corona Tracker',
-    tech: ['HTML', 'CSS', 'Javascript', 'React.js', 'Rest Api'],
-    link: 'https://github.com/akashgupta111/Covid-Tracker',
-    imgSrc: "https://drive.google.com/thumbnail?id=1z2ohX7PBIE79bVdTO9cWjhvr_TI9HoMM",
-    deployedLink: 'https://covid-tracker-ind.netlify.app/#/'
-}, {
-    name: 'Netflix Clone',
-    tech: ['HTML', 'CSS', 'Javascript', 'React.js', 'Rest Api'],
-    link: 'https://github.com/akashgupta1116/Netflix-Clone',
-    imgSrc: "https://drive.google.com/thumbnail?id=1QlFzVphGh6j1xaMny0RByxExW1025xTJ",
-    deployedLink: 'https://akashgupta1116.github.io/Netflix-Clone/#/'
-}, {
-    name: 'Havells Gallery',
-    tech: ['HTML', 'CSS', 'Javascript'],
-    link: 'https://github.com/akashgupta1116/Havells-Gallery',
-    imgSrc: "https://drive.google.com/thumbnail?id=1zWI2dfUtj3dKA--xnct1IXxszO95lV3Z",
-    deployedLink: 'https://royaltraders-havellsgallery.netlify.app/'
-}]
+const details = [
+  {
+    name: "Havells Gallery",
+    tech: ["HTML", "CSS", "Javascript"],
+    link: "https://github.com/akashgupta1116/Havells-Gallery",
+    imgSrc:
+      "https://drive.google.com/thumbnail?id=1zWI2dfUtj3dKA--xnct1IXxszO95lV3Z",
+    deployedLink: "https://royaltraders-havellsgallery.netlify.app/",
+  },
+  {
+    name: "Akash Gupta",
+    tech: ["HTML", "CSS", "Javascript", "React.js"],
+    link: "https://github.com/akashgupta111/Covid-Tracker",
+    imgSrc:
+      "https://drive.google.com/thumbnail?id=1rrWRSxIkP8v3spi-R64KNJMpwFx-aTJp",
+    deployedLink: "https://akashgupta-portfolio.netlify.app/#/",
+  },
+  {
+    name: "Realtime Voting App",
+    tech: ["HTML", "CSS", "Javascript", "React.js", "NodeJS", "ChartJS"],
+    link: "https://github.com/akashgupta1116/realtime-voting-reactjs",
+    imgSrc:
+      "https://drive.google.com/thumbnail?id=1c20PuvkW3XZrEHDUonDGp-bJ-HR5CmSs",
+    deployedLink: "https://realtime-voting-react-ably.herokuapp.com/",
+    isDeprecated: true,
+  },
+  {
+    name: "India Corona Tracker",
+    tech: ["HTML", "CSS", "Javascript", "React.js", "Rest Api"],
+    link: "https://github.com/akashgupta111/Covid-Tracker",
+    imgSrc:
+      "https://drive.google.com/thumbnail?id=1z2ohX7PBIE79bVdTO9cWjhvr_TI9HoMM",
+    deployedLink: "https://covid-tracker-ind.netlify.app/#/",
+    isDeprecated: true,
+  },
+  {
+    name: "Netflix Clone",
+    tech: ["HTML", "CSS", "Javascript", "React.js", "Rest Api"],
+    link: "https://github.com/akashgupta1116/Netflix-Clone",
+    imgSrc:
+      "https://drive.google.com/thumbnail?id=1QlFzVphGh6j1xaMny0RByxExW1025xTJ",
+    deployedLink: "https://akashgupta1116.github.io/Netflix-Clone/#/",
+    isDeprecated: true,
+  },
+];
 
-
-export default details
-
+export default details;
